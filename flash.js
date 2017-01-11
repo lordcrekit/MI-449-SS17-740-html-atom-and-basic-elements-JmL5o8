29961927aae5bd9fbe4a4293e1f3acee07eca7b7
William Norman <lordcrekit@gmail.com>
2017-01-10 21:22:07 -0500

    Passing all tests.

--- flash.js
+++ flash.js
@@ -1,16 +1,15 @@
-function flashtext(ele,col) {
-    var tmpColCheck = document.getElementById( ele ).style.color;
-
-    if (tmpColCheck === 'silver') {
-        document.getElementById( ele ).style.color = col;
-    } else {
-        document.getElementById( ele ).style.color = 'silver';
-    }
-} 
-
 setInterval(function() {
-    flashtext('ft0','red');
-    flashtext('ft1','blue');
-    flashtext('ft2','green');
+    var eles = document.getElementsByName('ft');
+    for (var i = 0; i < eles.length; i++) {
+        eles[i].style.color = getRandomColor();
+    }
 }, 500 ); //set an interval timer up to repeat the function
 
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
